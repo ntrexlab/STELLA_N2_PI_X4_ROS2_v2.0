d4f75088a02b036e8d75ac7a09269581830bdb83
ntrexlab <85467544+ntrexlab@users.noreply.github.com>
2023-07-05 13:25:54 +0900

    Update create_udev_rules.sh

--- stella_bringup/create_udev_rules.sh
+++ stella_bringup/create_udev_rules.sh
@@ -4,7 +4,7 @@ echo "remap the devices serial port(ttyUSBX, ttySX) to  ydlidar, AHRS, Motordriv
 echo "devices usb connection as /dev/YDLIDAR, /dev/AHRS, /dev/MW, /dev/BT  , check it using the command : ls -l /dev|grep -e ttyUSB -e ttyS0"
 echo "start copy stella.rules to  /etc/udev/rules.d/"
 echo "$HOME/colcon_ws/src/STELLA_RASPBERRYPI_ROS2/stella_bringup/stella.rules"
-sudo cp $HOME/colcon_ws/src/STELLA_RASPBERRYPI_ROS2/stella_bringup/stella.rules  /etc/udev/rules.d
+sudo cp $HOME/colcon_ws/src/STELLA_N2_PI_X4_ROS2_v2.0/stella_bringup/stella.rules  /etc/udev/rules.d
 echo " "
 echo "Restarting udev"
 echo ""
